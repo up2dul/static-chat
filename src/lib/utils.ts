b9bed081b40dcba11247d7dc8812d480bcecb1e5
abdul <up2dul@gmail.com>
2025-07-16 02:27:47 +0700

    feat(utils): create date parser func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function parseDate(dateString: string): string {
+  const date = new Date(dateString);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  if (date >= today) {
+    return date.toLocaleTimeString();
+  } else {
+    return date.toLocaleDateString();
+  }
+}
